3f38039644e116ab19619d265d9f5aa039b9b232
Bella <bella.bosak@uconn.edu>
2026-09-17 20:27:54 -0400

    Fiddled

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,7 +11,7 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.5,
-        n_estimators=200,
+        n_estimators=500,
         max_depth=12,
         subsample=2,
         min_samples_leaf=5,
