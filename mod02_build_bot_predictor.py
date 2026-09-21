f5b880f8a01fd36dc1e70edbda83c3c258385a2a
Bella <bella.bosak@uconn.edu>
2026-09-21 15:40:04 -0400

    Fixed values that were wrong

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,10 +10,10 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.5,
-        n_estimators=500,
+        learning_rate=0.01,
+        n_estimators=400,
         max_depth=1000,
-        subsample=2,
+        subsample=0.5,
         min_samples_leaf=5,
         random_state=seed
     )
