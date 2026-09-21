13ab6a3d953a257bdd54feab91d7db688fa60061
Bella <bella.bosak@uconn.edu>
2026-09-21 15:45:45 -0400

    Fiddled and figured out how to update values finally

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -13,8 +13,8 @@ def train_model(X, y, seed=seed):
         learning_rate=0.01,
         n_estimators=400,
         max_depth=5,
-        subsample=0.25,
-        min_samples_leaf=5,
+        subsample=0.5,
+        min_samples_leaf=4,
         random_state=seed
     )
     model.fit(X, y)
