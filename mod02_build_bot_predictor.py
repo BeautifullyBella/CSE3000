f9613d60f4b5d267776f69b05fe8407021aa1471
Bella <bella.bosak@uconn.edu>
2026-09-21 15:43:23 -0400

    Fiddled

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -13,7 +13,7 @@ def train_model(X, y, seed=seed):
         learning_rate=0.01,
         n_estimators=400,
         max_depth=5,
-        subsample=0.5,
+        subsample=0.25,
         min_samples_leaf=5,
         random_state=seed
     )
